fd8831a3889e11a6bdca481edd63fc8c20dc7305
HelloRuiLi <1229668446@qq.com>
2017-12-21 10:36:42 +0800

    remove annotation

--- app-server/index.ts
+++ app-server/index.ts
@@ -1,18 +1,18 @@
 import * as express from 'express';
 import * as path from 'path';
-// import * as rendertron from 'rendertron-middleware';
+import * as rendertron from 'rendertron-middleware';
 
 const app = express();
 
-// app.use((req: express.Request, _: any, next: any) => {
-//   console.log('url: ', req.url);
-//   console.log('user-agent: ', req.header('User-Agent'));
-//   next();
-// });
-//
-// app.use(rendertron.makeMiddleware({
-//   proxyUrl: 'http://localhost:3000/render',
-// }));
+app.use((req: express.Request, _: any, next: any) => {
+  console.log('url: ', req.url);
+  console.log('user-agent: ', req.header('User-Agent'));
+  next();
+});
+
+app.use(rendertron.makeMiddleware({
+  proxyUrl: 'http://localhost:3000/render',
+}));
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
